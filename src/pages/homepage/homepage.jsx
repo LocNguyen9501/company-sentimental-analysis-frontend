88ffa64e838d49dcc5093f47d9a8f660797d579a
Loc Nguyen <62735676+LocNguyen9501@users.noreply.github.com>
2023-03-29 22:32:08 -0400

    Add Data

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -4,13 +4,19 @@ import snap from "../../assets/snap2.png"
 import aws from "../../assets/aws.png"
 import roku from "../../assets/roku.png"
 import dropbox from "../../assets/dropbox.png"
+import mongoDB from "../../assets/mongoDB.png"
+import OCI from "../../assets/OCI.png"
+import SVN from "../../assets/servicenow.png"
+import SNFLK from "../../assets/snflk.png"
+import paypal from "../../assets/payapl.png"
+import microsoft from "../../assets/microsoft.png"
 
 export const HomePage = () => {
   
   return (
     <div className="homepage">
       <div className="companies">
-        {PRODUCTS.map((company) => (
+        {COMPANIES.map((company) => (
           <CompanyProfile 
             data={company} 
           />
@@ -20,7 +26,7 @@ export const HomePage = () => {
   )
 }
 
-const PRODUCTS = [
+const COMPANIES = [
   {
     id:1,
     companyname: "DropBox",
@@ -44,5 +50,41 @@ const PRODUCTS = [
     companyname: "Roku",
     rating: "5/10",
     companyimage: roku
-  }
+  },
+  {
+    id:5,
+    companyname: "MongoDB",
+    rating: "7/10",
+    companyimage: mongoDB
+  },
+  {
+    id:6,
+    companyname: "Oracle Cloud Infrastructure",
+    rating: "7/10",
+    companyimage: OCI
+  },
+  {
+    id:7,
+    companyname: "ServiceNow",
+    rating: "5/10",
+    companyimage: SVN
+  },
+  {
+    id:8,
+    companyname: "Snowflake",
+    rating: "6/10",
+    companyimage: SNFLK
+  },
+  {
+    id:9,
+    companyname: "PayPal",
+    rating: "5/10",
+    companyimage: paypal
+  },
+  {
+    id:10,
+    companyname: "Microsoft",
+    rating: "6/10",
+    companyimage: microsoft
+  },
 ];
