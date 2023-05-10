e428a4b7eb140e6b4e61fb9a98e1ba84906da8b3
Loc Nguyen <62735676+LocNguyen9501@users.noreply.github.com>
2023-05-09 21:55:56 -0400

    Add chart

--- src/componenets/Chart.jsx
+++ src/componenets/Chart.jsx
@@ -1,60 +1,105 @@
 import "./Chart.css";
-import React from "react";
-import { PieChart, Pie, Cell } from "recharts";
+import React, { useCallback, useState } from "react";
+import { PieChart, Pie, Sector } from "recharts";
 
 const data = [
-  { name: "Group A", value: 40 },
-  { name: "Group B", value: 30 },
-  { name: "Group C", value: 30 },
+  { name: "Happy", value: 40 },
+  { name: "Neutral", value: 30 },
+  { name: "Angry", value: 30 },
 ];
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  index
-}) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
 
   return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`${(percent * 100).toFixed(2)}%`}
+      </text>
+    </g>
   );
 };
 
 export default function Chart() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onPieEnter = useCallback(
+    (_, index) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
+
   return (
     <div className="container">
-        <PieChart width={1200} height={1200}>
+        <PieChart width={500} height={500} className="pieChart">
             <Pie
+                activeIndex={activeIndex}
+                activeShape={renderActiveShape}
                 data={data}
-                cx={window.innerWidth/2 - 50}
-                cy={160}
-                labelLine={false}
-                label={renderCustomizedLabel}
+                cx={280}
+                cy={180}
+                innerRadius={80}
                 outerRadius={120}
                 fill="#8884d8"
                 dataKey="value"
-            >
-                {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
-            </Pie>
+                onMouseEnter={onPieEnter}
+            />
         </PieChart>
     </div>
     
